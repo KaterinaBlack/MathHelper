f7a96535acf556cf66e50b2c1a9f219f72443a6f
Kate Kondrashova <Kate Kondrashova>
2016-07-19 14:31:52 +0300

    #good_code_1 соблюдаем Code Style!

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -15,16 +15,23 @@ namespace MathHelper
             string S = Console.ReadLine();
 
             if (S == "q")
+            {
                 return;
+            }
 
             int M = Int32.Parse(S);
-            long c1 = 1; int c2 = 0; int c3 = 0;
-            for(int i = 1; i <= M; i++)
+            long c1 = 1;
+            int c2 = 0;
+            int c3 = 0;
+
+            for (int i = 1; i <= M; i++)
             {
                 c1 *= i;
                 c2 += i;
                 if (i % 2 == 0)
+                {
                     c3 = i;
+                }
             }
 
             Console.WriteLine("Факториал числа {0} равен {1}", M, c1);
