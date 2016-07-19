e5087dbb6a02dc2dc5f6742c155eb79c312bacf7
Kate Kondrashova <Kate Kondrashova>
2016-07-19 16:24:04 +0300

    #good_code_4 Делаем методы достаточно короткими! Не перегружаем метод логикой. Метод решает ровно 1 задачу.

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -10,35 +10,46 @@ namespace MathHelper
     {
         static void Main(string[] args)
         {
+            int number;
+            if (TryGetNumberFromUser(out number))
+            {
+                long fuct = 1;
+                int summ = 0;
+                int maxEvenNumber = 0;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    fuct *= i;
+                    summ += i;
+                    if (i % 2 == 0)
+                    {
+                        maxEvenNumber = i;
+                    }
+                }
+
+                // Выводим результаты на консоль
+                Console.WriteLine("Факториал числа {0} равен {1}", number, fuct);
+                Console.WriteLine("Сумма от 1 до {0} равна {1}", number, summ);
+                Console.WriteLine("Максимальное четное число меньше {0} равно {1}", number, maxEvenNumber);
+
+                Console.ReadKey();
+            }
+            
+            
+        }
+
+        private static bool TryGetNumberFromUser(out int number)
+        {
             Console.WriteLine("Здравствуйте, вас приветствует математическая программа");
             Console.WriteLine("Пожалуйста, введите число.");
             string userInput = Console.ReadLine();
-
             if (userInput == "q")
             {
-                return;
+                number = -1;
+                return false;
             }
-
-            int number = Int32.Parse(userInput);
-            long fuct = 1;
-            int summ = 0;
-            int maxEvenNumber = 0;
-
-            for (int i = 1; i <= number; i++)
-            {
-                fuct *= i;
-                summ += i;
-                if (i % 2 == 0)
-                {
-                    maxEvenNumber = i;
-                }
-            }
-
-            Console.WriteLine("Факториал числа {0} равен {1}", number, fuct);
-            Console.WriteLine("Сумма от 1 до {0} равна {1}", number, summ);
-            Console.WriteLine("Максимальное четное число меньше {0} равно {1}", number, maxEvenNumber);
-
-            Console.ReadKey();
+            number = Int32.Parse(userInput);
+            return true;
         }
     }
 }
