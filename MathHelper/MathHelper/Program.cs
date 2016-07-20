c81fa65c60dba2adbfa8b19f8046bb65cc0b5a9a
Kate Kondrashova <Kate Kondrashova>
2016-07-20 14:59:54 +0300

    ПЛОХОЙ КОД! Метод выполняет сразу несколько задач.

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -13,35 +13,41 @@ namespace MathHelper
             int number;
             if (TryGetNumberFromUser(out number))
             {
-                // Производим вычисление факториала, суммы от 1 до N и максимального четного числа меньше N
-                long fuct = 1;
-                int summ = 0;
-                int maxEvenNumber = 0;
-
-                for (int i = 1; i <= number; i++)
-                {
-                    fuct *= i;
-                    summ += i;
-                    if (i % 2 == 0)
-                    {
-                        maxEvenNumber = i;
-                    }
-                }
+                long fact;
+                int summ, maxEvenNumber;                
+                CalculateFactorialAndSummAndMaxEvenNumber(number, out fact, out summ, out maxEvenNumber);
 
                 // Выводим результаты на консоль
-                Console.WriteLine("Факториал числа {0} равен {1}", number, fuct);
+                Console.WriteLine("Факториал числа {0} равен {1}", number, fact);
                 Console.WriteLine("Сумма от 1 до {0} равна {1}", number, summ);
                 Console.WriteLine("Максимальное четное число меньше {0} равно {1}", number, maxEvenNumber);
 
                 Console.ReadKey();
             }
+
+
+        }
+
+        private static void CalculateFactorialAndSummAndMaxEvenNumber(int number, out long fact, out int summ, out int maxEvenNumber)
+        {
             
-            
+            fact = 1;
+            summ = 0;
+            maxEvenNumber = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+                summ += i;
+                if (i % 2 == 0)
+                {
+                    maxEvenNumber = i;
+                }
+            }
         }
 
         private static bool TryGetNumberFromUser(out int number)
         {
-            // Запрашиваем число у пользователя
             Console.WriteLine("Здравствуйте, вас приветствует математическая программа");
             Console.WriteLine("Пожалуйста, введите число.");
             string userInput = Console.ReadLine();
