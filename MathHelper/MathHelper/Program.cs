d3509c041f08144c5499177e704d6c80103172fc
Kate Kondrashova <Kate Kondrashova>
2016-07-20 17:30:01 +0300

    #good_code_7 Программа не должна падать НИКОГДА. Обрабатывайте все ошибки пользователя!

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -10,19 +10,26 @@ namespace MathHelper
     {
         static void Main(string[] args)
         {
-            int number;
-            if (TryGetNumberFromUser(out number))
+            try
             {
-                long fact = GetFactorial(number);
-                int summ = GetSummFrom1ToN(number);
-                int maxEvenNumber = GetMaxEvanNumber(number);
+                int number;
+                if (TryGetNumberFromUser(out number))
+                {
+                    long fact = GetFactorial(number);
+                    int summ = GetSummFrom1ToN(number);
+                    int maxEvenNumber = GetMaxEvanNumber(number);
 
-                Console.WriteLine("Факториал числа {0} равен {1}", number, fact);
-                Console.WriteLine("Сумма от 1 до {0} равна {1}", number, summ);
-                Console.WriteLine("Максимальное четное число меньше {0} равно {1}", number, maxEvenNumber);
-
-                Console.ReadKey();
+                    Console.WriteLine("Факториал числа {0} равен {1}", number, fact);
+                    Console.WriteLine("Сумма от 1 до {0} равна {1}", number, summ);
+                    Console.WriteLine("Максимальное четное число меньше {0} равно {1}", number, maxEvenNumber);
+                    
+                }
             }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Что-то пошло не так. Экстренный выход.");
+            }
+            Console.ReadKey();
 
 
         }
@@ -60,35 +67,24 @@ namespace MathHelper
             return fact;
         }
 
-        private static void CalculateFactorialAndSummAndMaxEvenNumber(int number, out long fact, out int summ, out int maxEvenNumber)
-        {
-            
-            fact = 1;
-            summ = 0;
-            maxEvenNumber = 0;
-
-            for (int i = 1; i <= number; i++)
-            {
-                fact *= i;
-                summ += i;
-                if (i % 2 == 0)
-                {
-                    maxEvenNumber = i;
-                }
-            }
-        }
-
         private static bool TryGetNumberFromUser(out int number)
         {
             Console.WriteLine("Здравствуйте, вас приветствует математическая программа");
             Console.WriteLine("Пожалуйста, введите число. Для выхода введите q");
             string userInput = Console.ReadLine();
-            if (userInput == "q")
+            while (!Int32.TryParse(userInput, out number))
             {
-                number = -1;
-                return false;
+                if (userInput == "q")
+                {
+                    number = -1;
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильное значение. Пожалуйста, введите именно число. Для выхода нажмите q.");
+                    userInput = Console.ReadLine();
+                }
             }
-            number = Int32.Parse(userInput);
             return true;
         }
     }
